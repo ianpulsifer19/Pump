36e3fc76f2add2a042bf2c0c289d237fec6cd01c
Ian Pulsifer <s016572@students.lmsd.org>
2023-02-28 14:33:13 -0500

    Initial Commit

---
+++ Pump/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pump
+//
+//  Created by Ian Pulsifer (student LM) on 2/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pump/PumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PumpApp.swift
+//  Pump
+//
+//  Created by Ian Pulsifer (student LM) on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct PumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
