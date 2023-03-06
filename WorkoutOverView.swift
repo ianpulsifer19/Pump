415a364cb727cb484acbcc665cc07fd4d1ea59c5
Crosby Johnson <s017812@students.lmsd.org>
2023-03-06 14:33:19 -0500

    overview

---
+++ WorkoutOverView.swift
@@ -0,0 +1,30 @@
+//
+//  WorkoutOverView.swift
+//  
+//
+//  Created by Crosby Johnson (student LM) on 3/6/23.
+//
+
+import SwiftUI
+
+struct WorkoutOverView: View {
+    var body: some View {
+            Rectangle()
+                .foregroundColor(Color.blue)
+                .cornerRadius(10)
+                .frame(width: 200, height: 100)
+                .padding()
+        VStack{
+            Text("")
+            Text("Sets - \(exercise.sets)")
+            Text("Reps - \(exercise.reps)")
+            Text("Weight - \(exercise.weight)lbs")
+        }
+    }
+}
+
+struct WorkoutOverView_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutOverView()
+    }
+}
