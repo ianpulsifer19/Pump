4bb06fa796ecf2f7c64fe134e3107f73bde306a5
Max K <s018220@students.lmsd.org>
2023-03-08 12:31:35 -0500

    NotesView

---
+++ Pump/NotesView.swift
@@ -0,0 +1,29 @@
+//
+//  NotesView.swift
+//  Pump
+//
+//  Created by Max Keefe (student LM) on 3/8/23.
+//
+
+import SwiftUI
+import FirebaseAuth
+import FirebaseDatabase
+
+
+
+struct NotesView: View {
+   
+    @EnvironmentalObject var userInfo : UserInfo
+  
+    var body: some View {
+       
+        Text("\($userInfo.workout.exercise.name)")
+        Text("\($userInfo.workout.exercise.details)")
+        
+    }
+struct NotesView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotesView()
+    }
+}
+}
